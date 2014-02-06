58fc3c93e3f86d7db83b70fbb52db4d5a1af2787
David Matson <githubdavid@gmail.com>
2014-02-06 09:36:10 -0800

    Created new scripts to open and close More Info window.

---
+++ scripts/CloseWindowHaltScript.c
@@ -0,0 +1,4 @@
+CloseWindowHaltScript
+Close Window [ Current Window ]
+Halt Script
+February 6, 平成26 9:30:18 Budget Planner.fp7 - CloseWindowHaltScript -1-
---
+++ scripts/moreInfo.c
@@ -0,0 +1,7 @@
+moreInfo
+New Window [ Name: "More Info"; Left: Get ( WindowDesktopWidth ) - (Get ( WindowDesktopWidth ) / 2 ) ]
+Go to Layout [ “moreInfo” (budget) ]
+Adjust Window
+[ Resize to Fit ]
+Pause/Resume Script [ Indeﬁnitely ]
+February 6, 平成26 9:28:43 Budget Planner.fp7 - moreInfo -1-
